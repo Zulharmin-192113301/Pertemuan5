c440b0d553bad5b26f2c2e09daf39170efff6d2e
Zulharmin-192113301 <192113301@students.mikroskil.ac.id>
2022-01-04 14:47:24 +0700

    Latihan 5.7 - components props.children

---
+++ src/Main.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Card from './components/Card/Card';
+
+export default class Main extends Component {
+    render() {
+        return (
+            <Card>
+                <p>Ini adalah isi dari kartu yang sudah dibuat, tes dulu ya</p>
+            </Card>
+        )
+    }
+}
---
+++ src/components/Card/Card.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import './Card.css';
+
+export default class Home extends React.Component{
+    render() {
+        return (
+            <div className='kartu'>
+                <div className='kepala-kartu'>Kartu Ku</div>
+                <div className='badan-kartu'>{this.props.children}</div>
+            </div>
+        )
+    }
+}
