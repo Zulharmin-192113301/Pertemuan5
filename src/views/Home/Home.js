48af636e180dd9d1ab1b26fbc38bfe15deb99e91
Zulharmin-192113301 <192113301@students.mikroskil.ac.id>
2022-01-04 14:36:15 +0700

    Latihan 5.5 - state

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,13 +1,17 @@
 import React from 'react';
 
 export default class Home extends React.Component{
-    render(){
-        return (
-          <div>
-            <h2>Judul Halaman Home</h2>
-            <p>Hai namaku {this.props.nama}</p>
-          </div>
-        )
-      }
-
+  constructor(props){
+    super(props);
+    this.state = {nama : this.props.nama}
+  }
+  render() {
+    return (
+      <div>
+          <h2>ini adalah Judul halaman home</h2>
+          <p>Hai namaku adalah {this.state.nama}</p>
+          <button onClick={()=> this.setState({nama: "Yanto"})}>Ganti nama nya</button>
+      </div>
+    )
+  }
 }
