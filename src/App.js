0110d8bb3a18dd231678d442aba687187c1f8369
Zulharmin-192113301 <192113301@students.mikroskil.ac.id>
2022-01-04 14:41:14 +0700

    Latihan 5.6 - state konten dinamis

--- src/App.js
+++ src/App.js
@@ -1,20 +1,62 @@
 import React, { Component } from 'react';
+import './css/bootstrap.min.css';
 import './App.css';
 import Home from './views/Home/Home';
+import About from './views/About/About';
+import Help from './views/Help/Help';
 
-function Pesan(props) {
-  return <p>Hai {props.nama}</p>
-}
 
 class App extends Component {
-  render(){
-    return (
-      <div>
-        <Pesan nama='Budi'/>
-        <Home nama='Doni'/>
-      </div>
-    )
+  constructor() {
+    super();
+
+    this.state = {
+      tampil : 'home'
+    }
   }
-}
+
+  render() {
+    const Tampil = ()=> {
+      if (this.state.tampil == 'home')
+      return <Home nama="Budi Suryana" />
+      else if (this.state.tampil == 'about')
+      return <About/>
+      else if (this.state.tampil == 'help')
+      return <Help/>
+    }
+  
+  return (
+    <div>
+        <nav className='navbar navbar-expand-sm bg-dark navbar-secondary'>
+          <ul className='navbar-nav'>
+            <li className='nav-item'>
+              <a onClick={()=> this.setState({tampil : 'home'})} className='nav-link' href='#'>
+                Home
+              </a>
+            </li>
+
+            <li className='nav-item'>
+              <a onClick={()=> this.setState({tampil : 'about'})} className='nav-link' href='#'>
+              About
+              </a>
+            </li>
+
+            <li className='nav-item'>
+              <a onClick={()=> this.setState({tampil : 'help'})} className='nav-link' href='#'>
+               Help 
+              </a>
+            </li>
+
+          </ul>
+        </nav>
+
+        <Tampil />
+
+    </div>
+  )
+
+    }
+  }
+
 
 export default App;
---
+++ src/views/About/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Home extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Halaman Tentang nih</h1>
+            </div>
+        )
+    }
+}
