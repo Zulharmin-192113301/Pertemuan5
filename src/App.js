ddbabcb7180c2e4a2590140a5a2c46eef34df6af
Zulharmin-192113301 <192113301@students.mikroskil.ac.id>
2022-01-04 14:25:39 +0700

    Latihan 5.1 - function

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+function bilanghai(){
+  return "Hai Dunia"
+}
+
 class App extends Component {
- render() {
- return (
-     <div className="App">
-       <header className="App-header">
-         <img src={logo} className="App-logo" alt="logo" />
-         <h1 className="App-title">Welcome to React</h1>
-      </header>
-          <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-           </p>
-      </div>
-    );
+
+  bilanghalo() {
+    return " Hallo Dunia"
   }
+
+  render(){
+    return (
+      <>
+      <p>Bilang Hai : <b>{bilanghai()}</b></p>
+      <p>Bilang Hallo : <b>{this.bilanghalo()}</b></p>
+      </>
+    )
+  }
+
+
 }
 
 export default App;
